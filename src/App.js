25636098ba26959a55d4a3d36f3e85de9db99cc4
EL MUSTAPHA LAKHLOUFI <111161472+MstafaLkhloufi@users.noreply.github.com>
2023-01-23 17:29:00 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import Footer from './components/pages/Footer/Footer';
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+     <Navbar />
+     <Routes>
+          <Route  path='/' element={<Home />} /> 
+          <Route path='/services' element={<Services />} /> 
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+     </Routes>
+     <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
